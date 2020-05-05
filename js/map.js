36a05a9dfb5f7ae52f6b8fd4636f6320cbb84f32
Killisreal <64438408+Killisreal@users.noreply.github.com>
2020-05-06 01:54:27 +0300

    final relize

--- js/map.js
+++ js/map.js
@@ -1,17 +1,23 @@
 $(document).ready(function () {
-    var map = new google.maps.Map(document.getElementById('map'), {
-        center: { lat: -14.432, lng: -71.275 },
-        zoom: 15
-    });
-    var marker = new google.maps.Marker({
-        position: new google.maps.LatLng(-14.433, -71.275),
-        map: map
-    });
-    var infowindow = new google.maps.InfoWindow({
-        content: "Я фанат Артема Дзюбы"
-    });
-    google.maps.event.addListener(marker, 'click', function () {
-        infowindow.open(map, marker);
-    });
-
+    $("#map").height($(window).height())
 })
+
+var map
+
+    function initMap() {
+
+        var map = new google.maps.Map(document.getElementById('map'), {
+            center: { lat: -14.432, lng: -71.275 },
+            zoom: 15
+        });
+        var marker = new google.maps.Marker({
+            position: new google.maps.LatLng(-14.433, -71.275),
+            map: map
+        });
+        var infowindow = new google.maps.InfoWindow({
+            content: "Я фанат Артема Дзюбы"
+        });
+        google.maps.event.addListener(marker, 'click', function () {
+            infowindow.open(map, marker);
+        });
+    }
